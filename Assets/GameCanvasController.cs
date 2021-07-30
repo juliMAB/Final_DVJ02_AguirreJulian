bf5ebe7b68d365106ed3f6382127b93e1a23d597
juliMAB <julian6aguirre@hotmail.com>
2021-07-30 09:58:51 -0300

    Ingame HUB controller

---
+++ Assets/GameCanvasController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameCanvasController : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI time;
+
+    private void Start()
+    {
+        score.text = "Score: 0";
+        time.text = "Time:\n00:00"; 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            UpdateScore();
+        }
+    }
+    void UpdateScore()
+    {
+        string newScore= "Score: ";
+        newScore += "10";
+        score.text = newScore;
+    }
+}
