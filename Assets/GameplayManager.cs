197b8947ed980ee109c71aa5bb35c54450a946f7
juliMAB <julian6aguirre@hotmail.com>
2021-07-30 12:55:37 -0300

    Add GameplayManager

---
+++ Assets/GameplayManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    data Mydata;
+    float time;
+    [SerializeField]Player Player;
+    void EndGame()
+    {
+        DataLogger.Get().SaveData(Mydata);
+    }
+}
