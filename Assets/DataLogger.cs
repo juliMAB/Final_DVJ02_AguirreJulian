9bcc4a1cee5c25b5d90f31bfc83f36c69f65d0c9
juliMAB <julian6aguirre@hotmail.com>
2021-07-30 12:57:33 -0300

    Add DataLogger to save the gameplay data

---
+++ Assets/DataLogger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonoBehaviourSingletonScript;
+
+public class DataLogger : MonoBehaviourSingleton<DataLogger>
+{
+    data myData;
+    private void Start()
+    {
+        
+    }
+
+    public void SaveData(data data)
+    {
+        myData = data; //la info del player.
+    }
+}
