a5d34e0058bfa58109bd243e55086eb9e8d1ded7
juliMAB <julian6aguirre@hotmail.com>
2021-07-29 23:04:55 -0300

    Add MenuController to acces to sceneController

---
+++ Assets/scipts/sceneControllers/MenuController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using scene;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadGame() => sceneController.Get().LoadGame();
+
+    public void ExitGame() => sceneController.Get().ExitGame();
+}
