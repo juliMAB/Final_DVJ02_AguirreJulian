b4a37c86b2678cda18bfcba9379c2b7a3b7469bb
juliMAB <julian6aguirre@hotmail.com>
2021-07-29 23:32:52 -0300

    Add GameSceneController

---
+++ Assets/scipts/sceneControllers/GameSceneController.cs
@@ -0,0 +1,14 @@
+﻿using scene;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneController : MonoBehaviour
+{
+    
+    public void LoadMenu() => sceneController.Get().LoadMenu();
+
+    public void StopTime() => Time.timeScale = 0;
+
+    public void ResumeTime() => Time.timeScale = 1;
+}
