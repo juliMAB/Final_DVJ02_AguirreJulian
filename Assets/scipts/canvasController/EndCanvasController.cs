5079f5559bc98e8b74a394ada53d9a306e79c58d
juliMAB <julian6aguirre@hotmail.com>
2021-07-30 12:52:11 -0300

    Add EndCanvasController

---
+++ Assets/scipts/canvasController/EndCanvasController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndCanvasController : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI boxDestoys;
+    public TextMeshProUGUI tankDistance;
+    public TextMeshProUGUI winOrLose;
+}
