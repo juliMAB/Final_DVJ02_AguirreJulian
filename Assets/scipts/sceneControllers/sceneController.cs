a0dfd3b2da7aa6295556cc3780e62fb23cea48ba
juliMAB <julian6aguirre@hotmail.com>
2021-07-29 23:04:34 -0300

    Add sceneController singleton

---
+++ Assets/scipts/sceneControllers/sceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonoBehaviourSingletonScript;
+using UnityEngine.SceneManagement;
+
+namespace scene
+{
+    public class sceneController : MonoBehaviourSingleton<sceneController>
+    {
+        public void LoadGame()
+        {
+            SceneManager.LoadScene("GAME");
+        }
+        public void LoadMenu()
+        {
+            SceneManager.LoadScene("MENU");
+        }
+        public void LoadEnd()
+        {
+            SceneManager.LoadScene("END");
+        }
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
+
+
