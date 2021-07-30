3166bd659ecee336e9cb48e5f57abb1b2dc21dd4
juliMAB <julian6aguirre@hotmail.com>
2021-07-30 12:51:28 -0300

    Add data to save data

---
+++ Assets/data.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class data
+{
+    int score;
+    public int getScore => score;
+
+    int destroyBoxes;
+    
+    public int getDestroyBoxes => destroyBoxes;
+
+    float distanceTraveled;
+
+    public float getDistanceTraveled => distanceTraveled;
+}
