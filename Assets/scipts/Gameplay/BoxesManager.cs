638ef5e51e2a63654163ab626d17e6c556b37a7c
juliMAB <julian6aguirre@hotmail.com>
2021-07-31 13:34:38 -0300

    Add boxesManager to spawn boxes

---
+++ Assets/scipts/Gameplay/BoxesManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxesManager : MonoBehaviour
+{
+    public System.Action OnBoxCreated;
+    [SerializeField] int boxCuantity;
+    [SerializeField] GameObject box;
+    [SerializeField] GameObject spawner;
+    [SerializeField] GameObject conteinerBoxes;
+    [SerializeField] LayerMask TerrainLayer;
+    [SerializeField] public List <Box> boxes;
+
+    public void dependentStart()
+    {
+        for (int i = 0; i < boxCuantity; i++)
+        {
+            GameObject go= Instantiate(box, Vector3.zero, Quaternion.identity, conteinerBoxes.transform);
+            RandvecInTerrain(go);
+            boxes.Add(go.GetComponent<Box>());
+        }
+        OnBoxCreated?.Invoke();
+        Destroy(spawner);
+    }
+    void RandvecInTerrain(GameObject go)
+    {
+        RaycastHit hit;
+        float randomPositionX = spawner.transform.position.z + UnityEngine.Random.Range(-spawner.transform.localScale.x / 2, spawner.transform.localScale.x / 2);
+        float randomPositionZ = spawner.transform.position.z + UnityEngine.Random.Range(-spawner.transform.localScale.z / 2, spawner.transform.localScale.z / 2);
+        float randomPositionY = 0f;
+
+        if (Physics.Raycast(new Vector3(randomPositionX, 9999f, randomPositionZ), Vector3.down, out hit, Mathf.Infinity, TerrainLayer))
+        {
+            randomPositionY = hit.point.y + go.transform.localScale.y;
+            go.transform.position = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+            go.transform.rotation = Quaternion.LookRotation(hit.normal);
+            return;
+        }
+    }
+}
