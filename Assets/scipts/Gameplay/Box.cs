f68378ecc9b9986f7f1f06ac11648db551d2383a
juliMAB <julian6aguirre@hotmail.com>
2021-07-31 13:36:08 -0300

    Box action when die

--- Assets/scipts/Gameplay/Box.cs
+++ Assets/scipts/Gameplay/Box.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Box : MonoBehaviour, IKilleable
 {
+    public System.Action OnBoxKill;
     public void kill()
     {
+        OnBoxKill?.Invoke();
         Destroy(gameObject);
     }
 }
