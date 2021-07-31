9ddd2c3fc69821e246e29921a88f37ebb3e50596
juliMAB <julian6aguirre@hotmail.com>
2021-07-31 13:45:21 -0300

    GameplayManager start boxManager and actions on boxes

--- Assets/scipts/Gameplay/GameplayManager.cs
+++ Assets/scipts/Gameplay/GameplayManager.cs
@@ -1,14 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameplayManager : MonoBehaviour
 {
-    data Mydata;
-    float time;
-    [SerializeField]Player Player;
+    private Data Mydata = new Data();
+    public Action OnGameOver;
+    [Header("GameConfig")]
+    [SerializeField] float gameTime = 300; // 5 minutos.
+    [SerializeField] int scorePerKill;
+
+    [Header("dependencies")]
+    [SerializeField] ShipManager player;
+    [SerializeField] GameCanvasController canvas;
+    [SerializeField] BoxesManager boxesManager;
+    private void Start()
+    {
+        Mydata.ScorePerKill = scorePerKill;
+        boxesManager.OnBoxCreated += chargeBoxes;
+        boxesManager.dependentStart();
+    }
+    void UpdateCanvas()
+    {
+        canvas.ScoreF = Mydata.Score;
+        canvas.UpdateScore();
+    }
+    void chargeBoxes()
+    {
+
+        foreach (var item in boxesManager.boxes)
+        {
+            item.OnBoxKill += Mydata.OnBoxKill;
+            item.OnBoxKill += UpdateCanvas;
+        }
+    }
     void EndGame()
     {
+        Mydata.DistanceTraveled= (player.getShipController.TotalDistance);
         DataLogger.Get().SaveData(Mydata);
+
+    }
+    private void Update()
+    {
+        gameTime -= Time.deltaTime;
+        canvas.TimeF = gameTime;
+        canvas.UpdateTime();
     }
 }
