ab13771e7d34c4006869a8670bb9040cb15446d1
juliMAB <julian6aguirre@hotmail.com>
2021-07-31 13:35:31 -0300

    ShipManager to acces to gameplayManager to shipController

---
+++ Assets/scipts/Gameplay/ShipManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipManager : MonoBehaviour
+{
+    [SerializeField]ShipController shipController;
+    public ShipController getShipController => shipController;
+
+}
