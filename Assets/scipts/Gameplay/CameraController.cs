73a1b3e7ce2e4118e309eab44576f7cf0d85378b
juliMAB <julian6aguirre@hotmail.com>
2021-07-30 22:53:45 -0300

    Add CameraController

---
+++ Assets/scipts/Gameplay/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] Vector3 smothness;
+    [SerializeField] Vector3 rotare;
+
+    private void LateUpdate()
+    {
+        transform.position = target.transform.position + smothness;
+        transform.eulerAngles = rotare;
+    }
+}
