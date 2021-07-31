3fbd181f5355239ec2f4860dd1714d3976ad0422
juliMAB <julian6aguirre@hotmail.com>
2021-07-31 13:46:23 -0300

    Fix canvas isues

--- Assets/scipts/canvasController/GameCanvasController.cs
+++ Assets/scipts/canvasController/GameCanvasController.cs
@@ -5,18 +5,55 @@ using TMPro;
 
 public class GameCanvasController : MonoBehaviour
 {
-    public TextMeshProUGUI score;
-    public TextMeshProUGUI time;
+    [SerializeField] TextMeshProUGUI score;
+    [SerializeField] TextMeshProUGUI time;
 
+    private float timef;
+    public float TimeF { set { timef = value; } }
+    private int scoref;
+    public int ScoreF { set { scoref = value; } }
     private void Start()
     {
         score.text = "Score: 0";
         time.text = "Time:\n00:00"; 
     }
-    void UpdateScore()
+    public void UpdateScore()
     {
         string newScore= "Score: ";
-        newScore += "10";
+        newScore += scoref.ToString();
         score.text = newScore;
     }
+    public void UpdateTime()
+    {
+        string newTime = "Time:\n";
+        int min = (int)timef / 60;
+        int seg = (int)timef % 60;
+
+        if (min < 10)
+        {
+            newTime += "0" + min;
+            if (seg<10)
+            {
+                newTime += ":0" + seg;
+            }
+            else
+            {
+                newTime += ":"  + seg;
+            }
+        }
+        else
+        {
+            newTime += min;
+            if (seg < 10)
+            {
+                newTime += ":0" + seg;
+            }
+            else
+            {
+                newTime += ":" +  seg;
+            }
+        }
+        time.text = newTime;
+    }
+
 }
