723cf1b8c2f7a9eecaa5dc3768ce0634e0df8382
juliMAB <julian6aguirre@hotmail.com>
2021-08-01 03:48:54 -0300

    Add water, waterSplash particles and cloud rain

---
+++ Assets/scipts/Gameplay/FX/waterFx.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterFx : MonoBehaviour
+{
+    [SerializeField] GameObject WaterSpash;
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject go = Instantiate(WaterSpash, other.ClosestPoint(transform.position), Quaternion.identity, null);
+        go.name = "waterSplash";
+        Destroy(go, 1.5f);
+    }
+}
