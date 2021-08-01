8eed412004fb3f7bbb8e1f1250461d18847fbc7b
juliMAB <julian6aguirre@hotmail.com>
2021-08-01 13:15:39 -0300

    Add Enemy whit navMesh, follow player

---
+++ Assets/scipts/Gameplay/Manager/EnemyTank.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyTank : Tank
+{
+    [Header("EnemyTank")]
+    [SerializeField] PlayerFx playerFx;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator animator;
+    [SerializeField] LayerMask player;
+    [SerializeField] float distanceToFollow;
+    [SerializeField] float distanceToShoot;
+    Transform target;
+
+    void Start()
+    {
+        playerFx.OnShoot += Shoot;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((player & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+        {
+            target = other.transform;
+        }
+    }
+    void Update()
+    {
+        //if (Physics.SphereCast(transform.position, 10, transform.forward, out RaycastHit hit, 10))
+        //{
+        //    Debug.Log("Te veo perra");
+        //    agent.SetDestination(hit.point);
+        //    animator.SetBool("OnMove", true);
+        //    if (Vector3.Distance(transform.position, hit.point) <= distanceToShoot)
+        //    {
+        //        animator.SetBool("OnMove", false);
+
+        //        if (!CR_running)
+        //        {
+        //            Target = hit.point;
+        //            animator.Play("shoot");
+        //        }
+
+        //    }
+        //    else
+        //    {
+        //        animator.SetBool("OnMove", true);
+        //    }
+        //    FaceTarget(hit.point);
+        //}
+        //else
+        //{
+        //    animator.SetBool("OnMove", false);
+        //}
+        if (target!=null)
+        {
+            float distance = Vector3.Distance(target.position, transform.position);
+            if (distance <= distanceToFollow)
+            {
+                animator.SetBool("OnMove", true);
+                if (Vector3.Distance(transform.position, target.position) <= distanceToShoot)
+                {
+                    animator.SetBool("OnMove", false);
+
+                    if (!CR_running)
+                    {
+                        Target = target.position;
+                        animator.Play("shoot");
+                    }
+
+                }
+                else
+                {
+                    animator.SetBool("OnMove", true);
+                }
+                FaceTarget(target.position);
+                agent.SetDestination(target.position);
+            }
+            else
+            {
+                animator.SetBool("OnMove", false);
+            }
+        }
+        
+    }
+
+        void FaceTarget(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distanceToFollow);
+    }
+}
+
---
+++ Assets/scipts/Gameplay/Manager/Tank.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class Tank : MonoBehaviour,IHitable
+{
+    [Header("ClassTank")]
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject canonPoint;
+    [SerializeField] GameObject pivHead;
+    [SerializeField] float shootForce;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] int lives;
+    [SerializeField] int damage;
+    private Vector3 target;
+    public Vector3 Target { set { target = value; } get { return target; } }
+    private bool cr_running;
+    public bool CR_running { set { cr_running = value; } get { return cr_running; } }
+    public System.Action OnStartShoot;
+
+
+    public void Shoot()
+    {
+        GameObject go = Instantiate(bullet, canonPoint.transform.position, Quaternion.identity);
+        go.GetComponent<Rigidbody>().AddForce((target - transform.position) * shootForce, ForceMode.Impulse);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        lives-=damage;
+    }
+
+    public IEnumerator headRotate(Vector3 target)
+    {
+        CR_running = true;
+        Quaternion a = pivHead.transform.rotation;
+        Quaternion b = Quaternion.LookRotation(target - transform.position, transform.up);
+        float t = 0;
+        while (0.05f < Quaternion.Angle(pivHead.transform.rotation, b))
+        {
+            pivHead.transform.rotation = Quaternion.Lerp(a, b, t);
+            t += Time.deltaTime * rotationSpeed;
+            yield return null;
+        }
+        OnStartShoot?.Invoke();
+        CR_running = false;
+    }
+}
