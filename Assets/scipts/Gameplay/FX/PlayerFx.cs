f8ca58672fb47f5544c7ce09119316bf68895665
juliMAB <julian6aguirre@hotmail.com>
2021-07-31 23:11:37 -0300

    A couple of animations for my tank

---
+++ Assets/scipts/Gameplay/FX/PlayerFx.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFx : MonoBehaviour
+{
+    [SerializeField]GameObject explotion;
+    [SerializeField] GameObject canonPoint;
+    void Spawn_ShootEffect_1()
+    {
+        Instantiate(explotion,canonPoint.transform.position,Quaternion.identity,null);
+    }
+}
